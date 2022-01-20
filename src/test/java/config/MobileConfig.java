d1c8321f8c30cf5175bf4edc3dd1eace108abf94
dimidia379 <j.zvereva050@gmail.com>
2022-01-20 14:28:13 +0300

    Selenide test added

---
+++ src/test/java/config/MobileConfig.java
@@ -0,0 +1,10 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({"classpath:mobile.properties"})
+public interface MobileConfig extends Config {
+    String user();
+    String key();
+    String url();
+}
---
+++ src/test/java/helpers/Browserstack.java
@@ -0,0 +1,17 @@
+package helpers;
+
+import static io.restassured.RestAssured.given;
+
+public class Browserstack {
+    public static String videoUrl(String sessionId) {
+        return given()
+                .auth().basic("jz_Si6xIa", "zke7rdYARxokGopQ6ptL")
+                .when()
+                .get("https://api-cloud.browserstack.com/app-automate/sessions/" + sessionId +".json")
+                .then()
+                .statusCode(200)
+                .log().body()
+                .extract()
+                .path("automation_session.video_url");
+    }
+}
